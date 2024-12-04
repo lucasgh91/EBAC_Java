ed790ea9472cb264962021db949ef346dee4d044
Lucas G. <ghizzolucas@gmail.com>
2024-12-04 00:10:09 -0300

    Realizado exercício tarefa_classes

---
+++ Tarefa_classes/src/br/com/lucas/Gato.java
@@ -0,0 +1,35 @@
+package br.com.lucas;
+
+/**
+ * @author lucas
+ * @version 1.0
+ * @
+ */
+public class Gato {
+	
+	String nome;
+	int idade;
+	String cor;
+	String raca;
+	
+	/**
+	 * Método para fazer o gato dormir
+	 */
+	public void dormir() {
+		System.out.println("Dormindo");
+	}
+	
+	/**
+	 * Método para fazer o gato brincar
+	 */
+	public void brincar() {
+		System.out.println("Brincando");
+	}
+	
+	/**
+	 * Método para fazer o gato comer
+	 */
+	public void comer() {
+		System.out.println("Comendo");
+	}
+}
