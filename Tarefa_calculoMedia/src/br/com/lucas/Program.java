97e25baad5c7320bdae68f2864df5a12a8595b8c
Lucas G. <ghizzolucas@gmail.com>
2024-12-08 22:43:19 -0300

    Realizado exercício tarefa_calculoMedia

---
+++ Tarefa_calculoMedia/src/br/com/lucas/Program.java
@@ -0,0 +1,17 @@
+package br.com.lucas;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		double nota1 = 6.1;
+		double nota2 = 8.5;
+		double nota3 = 9.3;
+		double nota4 = 3.5;
+		
+		double media = (nota1 + nota2 + nota3 + nota4) / 4;
+		
+		System.out.println("A média é: " + media);
+	}
+
+}
